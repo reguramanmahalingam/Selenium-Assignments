e143841cc6bf8e41127cca562ef838bac50164aa
reguramanmahalingam <regumaha@yahoo.co.in>
2024-08-20 17:51:16 +0530

    Add files via upload

---
+++ Week 1/Day 2/LeanForLoop.java
@@ -0,0 +1,13 @@
+package week1.day2;
+
+public class LeanForLoop {
+	public static void main(String [] augs) {
+		
+		int num=7;
+		
+		for (int i = 0; i <num; i++) {
+			// for ctrl + space => 2nd option
+		}
+		
+	}
+}
---
+++ Week 1/Day 2/PrintEvenNumber.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class PrintEvenNumber {
+	public static void main(String [] augs) {
+		int value = 10;
+		
+		for (int i = 0; i < value; i++ ) {
+			if  (i%2==1) {
+				System.out.println("Odd number" + i);
+			}
+		}
+		
+	}
+	
+}
